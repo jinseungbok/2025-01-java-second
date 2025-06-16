6fd9f49c3ac47479d2849f411d873c7396c48856
jinseungbok <92.bokjs@gmail.com>
2025-06-16 18:00:20 +0900

    250616_study_ArrayListStudy

---
+++ src/ch15/ArrayListStudy.java
@@ -0,0 +1,13 @@
+package ch15;
+
+import java.util.*;
+
+public class ArrayListStudy {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(); // 다형성
+        // <String> 제네릭(Generic), 제한
+        // 오로지 String 객체 주소값만 담을 수 있는 컬렉션
+        Card card = new Card("", "");
+        // list.add(card); 안 담긴다.
+    }
+}
