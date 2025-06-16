ec8a1760529859892d01a9ce0eb89ee4539c1d45
jinseungbok <92.bokjs@gmail.com>
2025-06-16 17:20:22 +0900

    250616_study_blackjack

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -1,5 +1,7 @@
 package blackjack;
 
+import java.util.List;
+
 public class BlackJack {
     public static void main(String[] args) {
         CardDeckResult02 cd = new CardDeckResult02();
@@ -8,8 +10,30 @@ public class BlackJack {
         System.out.println(c);
         System.out.println(cd.getCard());
 
-        System.out.println("----------");
+        System.out.println("----------------");
         cd.printAllCards();
+
+        System.out.println("----Gamer card----");
+        Gamer gamer = new Gamer();
+        gamer.receiveCard(c);
+        gamer.receiveCard(cd.getCard());
+        gamer.receiveCard(cd.getCard());
+        gamer.showCards();
+        List<Card> cards = gamer.openCards();
+        int score = Rule.getScore(cards); // A:1, 2~10: 점수 그대로, J,Q,K: 10점
+        System.out.printf("score: %d\n", score);
+
+        System.out.println("----Dealer card----");
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(cd.getCard());
+        dealer.receiveCard(cd.getCard());
+        dealer.showCards();
+        System.out.printf("needMoreCard: %b\n", dealer.needMoreCard());
+        if(dealer.needMoreCard()) {
+            dealer.receiveCard(cd.getCard());
+        }
+        dealer.showCards();
+
     }
 }
 //      Object obj = cd;
