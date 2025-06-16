7e13c04824592e3a042b481dfcf43872b25c4f9a
jinseungbok <92.bokjs@gmail.com>
2025-06-16 18:02:30 +0900

    250616_study_ArrayListStudy

--- src/ch15/ArrayListStudy.java
+++ src/ch15/ArrayListStudy.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class ArrayListStudy {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(); // 다형성
+        List<String> list = new ArrayList<>(); // 다형성 - 오로지 상속관계만 가능
         // <String> 제네릭(Generic), 제한
         // 오로지 String 객체 주소값만 담을 수 있는 컬렉션
         Card card = new Card("", "");
