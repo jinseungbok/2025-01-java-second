fee4e8b7269b417e0b9c50dc60f315f9535bdd64
jinseungbok <92.bokjs@gmail.com>
2025-06-17 18:08:45 +0900

    250617_study_Array, Inheritance

--- src/ch07/sec07/poly/Animal.java
+++ src/ch07/sec07/poly/Animal.java
@@ -1,9 +1,10 @@
 package ch07.sec07.poly;
 
-public class Animal {
-    public void crying() {
-        System.out.println("동물이 운다");
-    }
+public abstract class Animal {
+    // abstract - 추상 메소드
+    // 추상 메소드를 단 하나라도 가지고 있다면 그 클래스는
+    // 추상 클래스가 되어야 한다.
+    public abstract void crying();
 }
-
+    // 구현부가 없어야 한다.
 
