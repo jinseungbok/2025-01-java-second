9b694934c08cabb4628d852b11bac332013542fc
jinseungbok <92.bokjs@gmail.com>
2025-06-19 18:04:37 +0900

    250619_study_interface, implements_study

---
+++ src/ch08/sec11/exam02/Vehicle.java
@@ -0,0 +1,12 @@
+package ch08.sec11.exam02;
+/*
+    interface - instance memberfield X, static memberfield O
+    static final memberfield O
+    무조건 추상 메소드만 가질 수 있음
+    public abstract 생략 가능
+    interface - public
+ */
+public interface Vehicle {
+    void run();
+    void stop();
+}
