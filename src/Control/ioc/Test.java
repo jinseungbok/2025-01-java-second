cbd749c13274404c212a652832721e95acf224bd
jinseungbok <92.bokjs@gmail.com>
2025-06-20 17:54:27 +0900

    250620_interface_study

--- src/Control/ioc/Test.java
+++ src/Control/ioc/Test.java
@@ -6,7 +6,9 @@ public class Test {
     public static void main(String[] args) {
         TvFactory tvFactory = TvFactory.getInstance();
 //      TvFactory tvFactory1 = new TvFactory(); // 안되도록 변경
-        Tv tv = tvFactory.factory("harman", "genelec");
+//      Tv tv = tvFactory.factory("harman", "genelec");
+        Tv tv = tvFactory.factory(args[0], args[1]);
+        // Run/Debug configurations 이용해 변경 가능
         tv.sound();
     }
 }
