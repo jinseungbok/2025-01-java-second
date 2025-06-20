1c47145603f679fda4f0e93ee3efad8f96059968
jinseungbok <92.bokjs@gmail.com>
2025-06-20 17:50:17 +0900

    250620_interface_study

--- src/Control/ioc/Test.java
+++ src/Control/ioc/Test.java
@@ -1,9 +1,12 @@
 package Control.ioc;
 
+import Control.Tv;
+
 public class Test {
     public static void main(String[] args) {
         TvFactory tvFactory = TvFactory.getInstance();
 //      TvFactory tvFactory1 = new TvFactory(); // 안되도록 변경
+        Tv tv = tvFactory.factory("harman", "genelec");
+        tv.sound();
     }
-
 }
